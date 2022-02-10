613bf9c07e3018e1fa1a0c8b86c23d0ee14da1ad
Nishant Mehta <42231185+nishme0708@users.noreply.github.com>
2022-02-10 17:31:14 +0530

    Add router behaviour

---
+++ src/Link.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Link = ({ className, href, children }) => {
+    const onClickHandler = (e) => {
+        e.preventDefault();
+        window.history.pushState({}, '', href);
+        const navEvent = new PopStateEvent('popstate');
+        window.dispatchEvent(navEvent);
+    };
+    return (
+        <a href={href} className={className} onClick={onClickHandler}>
+            {children}
+        </a>
+    );
+};
+
+export default Link;
--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,14 @@
+import { useEffect,useState } from 'react';
+
 const Router = ({ path, children }) => {
-    return window.location.pathname === path ? children : null;
+    const [winPath,setPath] = useState(window.location.pathname);
+    const onLocationChange = ()=>{
+        setPath(window.location.pathname);
+    };
+    useEffect(()=>{
+        window.addEventListener('popstate',onLocationChange)
+    },[]);
+    return winPath === path ? children : null;
 };
 
 export default Router;
