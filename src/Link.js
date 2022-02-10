248e395297d96de30bc1ed3b01d39390d2ef900a
Nishant Mehta <42231185+nishme0708@users.noreply.github.com>
2022-02-10 17:32:37 +0530

    Add support for ctrl click

--- src/Link.js
+++ src/Link.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 const Link = ({ className, href, children }) => {
     const onClickHandler = (e) => {
+        if(e.metaKey || e.ctrlKey) {
+            return;
+        }
         e.preventDefault();
         window.history.pushState({}, '', href);
         const navEvent = new PopStateEvent('popstate');
