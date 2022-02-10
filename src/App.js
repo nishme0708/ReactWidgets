1bd3c7fa5e50f1f460c41a2714f31a7ea0b388e9
Nishant Mehta <42231185+nishme0708@users.noreply.github.com>
2022-02-10 13:22:33 +0530

    Add custom router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Accordion from './Accordion';
 import Search from './Search';
 import Dropdown from './Dropdown';
 import Translate from './Translate';
+import Router from './Router';
 
 const App = () => {
     let items = [
@@ -37,11 +38,58 @@ const App = () => {
             value: 'yellow'
         }
     ];
-    
+
+    const showAccordion = () => {
+        if (window.location.pathname === '/') {
+            return <Accordion items={items} />;
+        }
+    };
+    const showDropDown = () => {
+        if (window.location.pathname == '/dropdown') {
+            return <Dropdown options={dropdownItems} selected={colorState} onSelect={setColorState} />;
+        }
+    };
+
+    const showTranslate = () => {
+        if (window.location.pathname === '/translate') {
+            return <Translate />;
+        }
+    };
+
+    const showList = () => {
+        if (window.location.pathname === '/list') {
+            return <Search />;
+        }
+    };
+
     let [ colorState, setColorState ] = useState(dropdownItems[0]);
-    // return <Accordion items={items} />;
-    // return <Dropdown options={dropdownItems} selected={colorState} onSelect={setColorState}/>;
-    return <Translate/>;
+    return (
+        <div>
+            {
+                <Router path='/'>
+                    <Accordion items={items} />
+                </Router>
+            }
+
+            {
+                <Router path='/list'>
+                    <Search />
+                </Router>
+            }
+
+            {
+                <Router path='/translate'>
+                    <Translate />{' '}
+                </Router>
+            }
+
+            {
+                <Router path='/dropdown'>
+                    <Dropdown options={dropdownItems} selected={colorState} onSelect={setColorState} />
+                </Router>
+            }
+        </div>
+    );
 };
 
 export default App;
---
+++ src/Router.js
@@ -0,0 +1,5 @@
+const Router = ({ path, children }) => {
+    return window.location.pathname === path ? children : null;
+};
+
+export default Router;
